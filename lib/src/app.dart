8891b3e7aafb38a6bdaeeb2b8b2a815fad4bf861
Alexander Garuba <alexandergaruba96@gmail.com>
2020-07-10 21:00:22 +0100

    init commit

---
+++ lib/src/app.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'dart:convert';
+import 'models/images_models.dart';
+import 'widgets/image_list.dart';
+
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    counter++;
+    final response =
+        await get('https://jsonplaceholder.typicode.com/photos/$counter');
+    final imageModel = ImageModel.fromJson(json.decode(response.body));
+
+    setState(() {
+      images.add(imageModel);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Let\'s see some images!'),
+        ),
+        body: ImageList(images),
+        floatingActionButton: FloatingActionButton(
+          onPressed: fetchImage,
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/widgets/image_list.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import '../models/images_models.dart';
+
+class ImageList extends StatelessWidget {
+  final List<ImageModel> images;
+
+  ImageList(this.images);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: images.length,
+      itemBuilder: (context, int index) {
+        return buildImage(images[index]);
+      },
+    );
+  }
+
+  Widget buildImage(ImageModel image) {
+    return Container(
+      padding: EdgeInsets.all(20.0),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+      ),
+      margin: EdgeInsets.all(20.0),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(bottom: 10.0),
+            child: Image.network(image.url),
+          ),
+          Text(image.title),
+        ],
+      ),
+    );
+  }
+}
